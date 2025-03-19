f624eef1c151b9cb58015c6e9f2786fba61bea92
PauloHDuque <phbduque@hotmail.com>
2025-03-19 11:17:09 -0300

    refactor: upload banco de dados

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,29 +1,43 @@
 const { ProdutosFiscais } = require('../models');
+const { Op } = require('sequelize');
+
 
 async function uploadData(req, res) {
   try {
     const data = req.body;
     console.log('Data received:', data);
 
-    const uniqueData = [];
-    for (const item of data) {
-      const existingRecord = await ProdutosFiscais.findOne({
-        where: { NCM: item.NCM, CRT: item.CRT }
-      });
-      if (!existingRecord) {
-        uniqueData.push(item);
-      } else {
-        console.log(`Duplicate NCM with CRT found: ${item.NCM}, ${item.CRT}`);
-      }
-    }
+    // Extrair os pares de NCM e CRT únicos do payload
+    const ncmCrtPares = data.map(item => ({
+      NCM: item.NCM,
+      CRT: item.CRT,
+    }));
+
+    // Buscar no banco de dados os registros já existentes em uma única consulta
+    const registroExistente = await ProdutosFiscais.findAll({
+      where: {
+        [Op.or]: ncmCrtPares,
+      },
+    });
+
+    // Criar um conjunto de pares NCM+CRT já existentes para fácil comparação
+    const conjuntoExistente = new Set(
+      registroExistente.map(record => `${record.NCM}-${record.CRT}`)
+    );
+
+    // Filtrar os dados únicos que não estão presentes no conjunto existente
+    const uniqueData = data.filter(
+      item => !conjuntoExistente.has(`${item.NCM}-${item.CRT}`)
+    );
 
     console.log('Unique Data:', uniqueData);
 
+    // Inserir apenas os registros novos no banco de dados
     if (uniqueData.length > 0) {
       const result = await ProdutosFiscais.bulkCreate(uniqueData, { validate: true });
       console.log('Result:', result);
     } else {
-      console.log('No new records to insert.');
+      console.log('Sem novos registros para inserir');
     }
 
     res.json({
@@ -31,7 +45,7 @@ async function uploadData(req, res) {
       processedData: uniqueData,
     });
   } catch (error) {
-    console.error('Error while processing data:', error);
+    console.error('Error while processing data:', error.message, error.stack);
     res.status(500).json({ message: 'Error while processing data' });
   }
 }
--- src/components/conversor/index.jsx
+++ src/components/conversor/index.jsx
@@ -10,51 +10,100 @@ export default function Conversor() {
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     if (!file) return;
-
+  
     const reader = new FileReader();
     reader.onload = (e) => {
       const data = new Uint8Array(e.target.result);
       const workbook = XLSX.read(data, { type: "array" });
-      const sheetName = "NCMs_ENCONTRADOS";
-      const sheet = workbook.Sheets[sheetName];
-
-      if (!sheet) {
-        alert(`A aba '${sheetName}' não foi encontrada no arquivo.`);
+  
+      const possibleSheets = ["NCMs_ENCONTRADOS", "GTINs_ENCONTRADOS"];
+      let selectedSheet = null;
+  
+      for (const sheetName of possibleSheets) {
+        if (workbook.Sheets[sheetName]) {
+          selectedSheet = sheetName;
+          break;
+        }
+      }
+  
+      if (!selectedSheet) {
+        alert(`Nenhuma das abas esperadas foi encontrada no arquivo.`);
         return;
       }
-      const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 }).slice(6);
-      const formattedData = rawData.map((row) => ({
-        NCM: row[0] ? row[0].replace(/\./g, "") : "",
-        DESCRICAO: row[1] || "",
-        CEST: row[2] || "",
-        ALIQUOTA_IPI: row[3] ? row[3].replace(",", ".") : "",
-        CST_IPI: row[4] || "",
-        EX: row[5] || "",
-        CST_PIS_COFINS_ENTRADA: row[6] || "",
-        CST_PIS_COFINS_SAIDA: row[7] || "",
-        CODIGO_SPED: row[8] || "",
-        ALIQUOTA_PIS: row[9] ? row[9].replace(",", ".") : "",
-        ALIQUOTA_COFINS: row[10] ? row[10].replace(",", ".") : "",
-        CFOP: row[12] ? row[12].slice(-4) : "",
-        CST_CSOSN:
-          crtValue !== "01" ? (row[13] ? "0" + row[13] : "") : row[13] || "",
-        AD_REM_ICMS: row[14] || "",
-        ALIQUOTA_ICMS: row[15] ? row[15].replace(",", ".") : "",
-        RED_BASE_DE_CALCULO_ICMS: row[16] ? row[16].replace(",", ".") : "",
-        RED_BASE_DE_CALCULO_ICMS_ST: row[17] ? row[17].replace(",", ".") : "",
-        ALIQUOTA_ICMS_ST: row[18] ? row[18].replace(",", ".") : "",
-        ALIQUOTA_RED_BASE_DE_CALCULO_ICMS: row[19]
-          ? row[19].replace(",", ".")
-          : "",
-        MVA: row[20] ? row[20].replace(",", ".") : "",
-        FCP: row[21] ? row[21].replace(",", ".") : "",
-        COD_BENEFICIO_FISCAL: row[22] || "",
-        ANTECIPADO: row[23] || "",
-        PERCENTUAL_DIFERIMENTO: row[24] ? row[24].replace(",", ".") : "",
-        PERCENTUAL_ISENCAO: row[25] ? row[25].replace(",", ".") : "",
-        CODIGO_ANP: row[26] || "",
-        CRT: crtValue,
-      }));
+  
+      const sheet = workbook.Sheets[selectedSheet];
+      const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 }).slice(1);
+  
+      let formattedData;
+  
+      if (selectedSheet === "GTINs_ENCONTRADOS") {
+        formattedData = rawData.map((row) => ({
+          GTIN: row[0] || "",
+          NCM: row[2] ? row[2].replace(/\./g, "") : "",
+          DESCRICAO: row[1] || "",
+          CEST: row[3] || "",
+          ALIQUOTA_IPI: row[4] ? row[4].replace(",", ".") : "",
+          CST_IPI: row[5] || "",
+          EX: "",
+          CST_PIS_COFINS_ENTRADA: row[6] || "",
+          CST_PIS_COFINS_SAIDA: row[7] || "",
+          CODIGO_SPED: row[8] || "",
+          ALIQUOTA_PIS: row[9] ? row[9].replace(",", ".") : "",
+          ALIQUOTA_COFINS: row[10] ? row[10].replace(",", ".") : "",
+          CFOP: row[12] ? row[12].slice(-4) : "",
+          CST_CSOSN:
+            crtValue !== "01" ? (row[13] ? "0" + row[13] : "") : row[13] || "",
+          AD_REM_ICMS: row[14] || "",
+          ALIQUOTA_ICMS: row[15] ? row[15].replace(",", ".") : "",
+          RED_BASE_DE_CALCULO_ICMS: row[18] ? row[18].replace(",", ".") : "",
+          RED_BASE_DE_CALCULO_ICMS_ST: row[19] ? row[19].replace(",", ".") : "",
+          ALIQUOTA_ICMS_ST: row[20] ? row[20].replace(",", ".") : "",
+          ALIQUOTA_RED_BASE_DE_CALCULO_ICMS: row[16]
+            ? row[16].replace(",", ".")
+            : "",
+          MVA: row[21] ? row[21].replace(",", ".") : "",
+          FCP: row[22] ? row[22].replace(",", ".") : "",
+          COD_BENEFICIO_FISCAL: row[23] || "",
+          ANTECIPADO: row[24] || "",
+          PERCENTUAL_DIFERIMENTO: row[26] ? row[26].replace(",", ".") : "",
+          PERCENTUAL_ISENCAO: row[27] ? row[27].replace(",", ".") : "",
+          CODIGO_ANP: row[28] || "",
+          CRT: crtValue,
+        }));
+      } else if (selectedSheet === "NCMs_ENCONTRADOS") {
+        formattedData = rawData.map((row) => ({
+          NCM: row[2] ? row[2].replace(/\./g, "") : "",
+          DESCRICAO: row[1] || "",
+          CEST: row[2] || "",
+          ALIQUOTA_IPI: row[3] ? row[3].replace(",", ".") : "",
+          CST_IPI: row[4] || "",
+          EX: row[5] || "",
+          CST_PIS_COFINS_ENTRADA: row[6] || "",
+          CST_PIS_COFINS_SAIDA: row[7] || "",
+          CODIGO_SPED: row[8] || "",
+          ALIQUOTA_PIS: row[9] ? row[9].replace(",", ".") : "",
+          ALIQUOTA_COFINS: row[10] ? row[10].replace(",", ".") : "",
+          CFOP: row[12] ? row[12].slice(-4) : "",
+          CST_CSOSN:
+            crtValue !== "01" ? (row[13] ? "0" + row[13] : "") : row[13] || "",
+          AD_REM_ICMS: row[14] || "",
+          ALIQUOTA_ICMS: row[15] ? row[15].replace(",", ".") : "",
+          RED_BASE_DE_CALCULO_ICMS: row[16] ? row[16].replace(",", ".") : "",
+          RED_BASE_DE_CALCULO_ICMS_ST: row[17] ? row[17].replace(",", ".") : "",
+          ALIQUOTA_ICMS_ST: row[18] ? row[18].replace(",", ".") : "",
+          ALIQUOTA_RED_BASE_DE_CALCULO_ICMS: row[19]
+            ? row[19].replace(",", ".")
+            : "",
+          MVA: row[20] ? row[20].replace(",", ".") : "",
+          FCP: row[21] ? row[21].replace(",", ".") : "",
+          COD_BENEFICIO_FISCAL: row[22] || "",
+          ANTECIPADO: row[23] || "",
+          PERCENTUAL_DIFERIMENTO: row[24] ? row[24].replace(",", ".") : "",
+          PERCENTUAL_ISENCAO: row[25] ? row[25].replace(",", ".") : "",
+          CODIGO_ANP: row[26] || "",
+          CRT: crtValue,
+        }));
+      }
 
       setJsonData(formattedData);
       const tamanhoEmMB = (
@@ -139,17 +188,18 @@ export default function Conversor() {
         disabled={!crtValue}
         className={styles.fileInput}
       />
-      {loading && <div className={styles.loader}></div>}
-      {jsonData && (
-        <>
-          <button onClick={clearData} className={styles.clearButton}>
-            Limpar Dados
-          </button>
-          <pre className={styles.jsonOutput}>
-            {JSON.stringify(jsonData, null, 2)}
-          </pre>
-        </>
-      )}
+      {loading && (
+  <div className={styles.loader}>
+  </div>
+)}
+{!loading && jsonData && (
+  <>
+    <p className={styles.successMessage}>Dados enviados com sucesso!</p>
+    <button onClick={clearData} className={styles.clearButton}>
+      Limpar Dados
+    </button>
+  </>
+)}
     </div>
   );
 }
